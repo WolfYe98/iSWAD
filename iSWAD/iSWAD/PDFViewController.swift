453b421c19519688edb6bc4b2a6082b4f9111bd3
WolfYe98 <mingye@hotmail.es>
2021-03-23 20:53:21 +0100

    Bate Ye: Created PDFViewController for show pdfs

--- iSWAD/iSWAD/PDFViewController.swift
+++ iSWAD/iSWAD/PDFViewController.swift
@@ -7,24 +7,41 @@
 //
 
 import UIKit
+import PDFKit
 
 class PDFViewController: UIViewController {
 
+    @IBOutlet weak var miview: UIView!
+    @IBOutlet weak var milabel: UILabel!
+    var url: URL?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Debes actualizar el sistema a iOS 11 o superior para poder visualizar los PDFs.
         // Do any additional setup after loading the view.
+        milabel.isHidden = true
+        miview.backgroundColor = .black
+        if #available(iOS 11.0, *) {
+            let pdfview = PDFView()
+            pdfview.backgroundColor = .black
+            pdfview.translatesAutoresizingMaskIntoConstraints = false
+            miview.addSubview(pdfview)
+            pdfview.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            pdfview.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            pdfview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            pdfview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+            if url != nil{
+                if let documento = PDFDocument(url:url!){
+                    pdfview.document = documento
+                }
+            }
+            
+        } else {
+            milabel.isHidden = false
+            milabel.numberOfLines = 20
+            milabel.textAlignment = .center
+            
+            milabel.text = " Debes actualizar el sistema a iOS 11 o superior para poder visualizar los PDFs. "
+        
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
