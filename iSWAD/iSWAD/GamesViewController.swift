49e2b2d06bfa1917ea196b51e224f1f56f731ed9
WolfYe98 <mingye@hotmail.es>
2021-03-25 20:32:38 +0100

    Bate Ye: Reload table correctly

--- iSWAD/iSWAD/GamesViewController.swift
+++ iSWAD/iSWAD/GamesViewController.swift
@@ -24,22 +24,43 @@ class GamesViewController: UIViewController {
         textoInformativo.numberOfLines = 10
         textoInformativo.text = "Desliza hacia abajo\npara traer los juegos"
         refresh.attributedTitle = NSAttributedString(string: "Cargando")
-        refresh.addTarget(self, action: #selector(self.traerDatos(_:)), for: .valueChanged)
+        refresh.addTarget(self, action: #selector(self.getData(_:)), for: .valueChanged)
         self.tablaJuegos.addSubview(refresh)
         self.tablaJuegos.register(UINib(nibName: "IconTableViewCell", bundle: nil), forCellReuseIdentifier: "IconCell")
         self.tablaJuegos.dataSource = self
     }
-    // Crea una petición SOAP al servidor, en caso de que no se devuelva ningún error, actualiza la tabla con los juegos, en caso contrario, muestra una alerta.
     
-    @objc func traerDatos(_ sender:AnyObject){
+    // Crea una petición SOAP al servidor, en caso de que no se devuelva ningún error, actualiza la tabla con los juegos, en caso contrario, muestra una alerta.
+    // This method create a SOAP Request to the SWAD Server and if it doesn't return any error, updates the tableView with all games returned by SWAD Server and in the other case, it show an alert to the user.
+    @objc func getData(_ sender:AnyObject){
         let defaults = UserDefaults.standard
         let request = GetGames()
         let client = SyedAbsarClient()
         request.cpWsKey = defaults.string(forKey: Constants.wsKey)
         request.cpCourseCode = self.courseCode!
-        client.opGetGames(request){ (error,handler) in
+        client.opGetGames(request){ (error,response) in
             if error == nil{
-                self.textoInformativo.removeFromSuperview()
+                let gamesArray = response!["getGamesOutput"]["gamesArray"].children
+                let game = Game()
+                for item in gamesArray{
+                    game.gameCode = Int((item["gameCode"].element?.text)!)
+                    game.userSurname1 = (item["userSurname1"].element?.text)!
+                    game.userSurname2 = (item["userSurname2"].element?.text)!
+                    game.userFirstName = (item["userFirstname"].element?.text)!
+                    game.userPhoto = (item["userPhoto"].element?.text)!
+                    game.startTime = Int32((item["startTime"].element?.text)!)
+                    game.endTime = Int32((item["endTime"].element?.text)!)
+                    game.title = (item["title"].element?.text)!
+                    game.text = (item["text"].element?.text)!
+                    game.numQuestions = Int((item["numQuestions"].element?.text)!)
+                    game.maxGrade = Float((item["maxGrade"].element?.text)!)
+                    
+                    //add to games array
+                    self.games.append(game)
+                }
+                DispatchQueue.main.async {
+                    self.textoInformativo.removeFromSuperview()
+                }
             }
             else{
                 DispatchQueue.main.sync {
@@ -47,7 +68,7 @@ class GamesViewController: UIViewController {
                 }
             }
         }
-        DispatchQueue.main.async {
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
             self.tablaJuegos.reloadData()
             self.refresh.endRefreshing()
         }
