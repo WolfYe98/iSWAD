374dc2543cf9c23f802bef2fa18826a9fd8a0dd3
WolfYe98 <mingye@hotmail.es>
2021-03-26 22:40:53 +0100

    Bate Ye: Completed Matches view controller with table view

--- iSWAD/iSWAD/MatchesViewController.swift
+++ iSWAD/iSWAD/MatchesViewController.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class MatchesViewController: UIViewController {
 
+    var matches : [Match] = []
+    var courseCode : Int?
+    var gameCode : Int?
+    
     @IBOutlet weak var tablaPartidas: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tablaPartidas.register(UINib(nibName: "IconTableViewCell", bundle: nil), forCellReuseIdentifier: "IconCell")
+        self.tablaPartidas.dataSource = self
+        self.tablaPartidas.tableFooterView = UIView()
     }
+    
+}
+
+
+extension MatchesViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.matches.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tablaPartidas.dequeueReusableCell(withIdentifier: "IconCell") as! IconTableViewCell
+        cell.title.text = matches[indexPath.row].title!
+        
+        cell.notaMaxima.text = ""
+        
+        let startTime = unixTimeToString(unixTimeStamp: matches[indexPath.row].startTime!)
+        let endTime = unixTimeToString(unixTimeStamp: matches[indexPath.row].endTime!)
+        cell.time.text = "\(startTime) - \(endTime)"
+        
+        cell.icon.font = UIFont.fontAwesome(ofSize: 28)
+        cell.icon.text = String.fontAwesomeIcon(name: .checkSquareO)
+        
+        return cell
+    }
+    
     
 }
 
