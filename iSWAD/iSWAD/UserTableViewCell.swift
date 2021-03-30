44d9244e810cec50700ffe22007e0a72b30fae56
WolfYe98 <mingye@hotmail.es>
2021-03-30 23:09:04 +0200

    Bate Ye: created cells for users

--- iSWAD/iSWAD/UserTableViewCell.swift
+++ iSWAD/iSWAD/UserTableViewCell.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
-class UserTableViewCell: UITableViewCell {
 
+class UserTableViewCell: UITableViewCell {
+    var userSelected = false
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var id: UILabel!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var checkBox: CheckBox!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.name.numberOfLines = 4
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    @IBAction func checked(_ sender: Any) {
+        self.userSelected = !self.userSelected
+        self.checkBox.isChecked = self.userSelected
     }
     
 }
