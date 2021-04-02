e4e9a0288d0bb8058c5712fcb0e8f1c2de5f1bce
WolfYe98 <mingye@hotmail.es>
2021-04-02 20:11:42 +0200

    Bate Ye: created new viewController for read students Qr codes

--- iSWAD/iSWAD/QrReaderViewController.swift
+++ iSWAD/iSWAD/QrReaderViewController.swift
@@ -5,26 +5,148 @@
 //  Created by Bate Ye on 2/4/21.
 //  Copyright © 2021 Adrián Lara Roldán. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
+
+protocol QrReaderViewControllerDelegate: AnyObject{
+    func codeReaded(_ codes: [String])
+}
 
 class QrReaderViewController: UIViewController {
 
+    var captureSession : AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var qrView :UIView!
+    var codes = [String]()
+    var readedCodes = [String]()
+    
+    weak var delegate : QrReaderViewControllerDelegate?
+    
+    @IBOutlet weak var QrviewBounds: UIView!
+    // Overrides functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+        
+        //Creating a capture session
+        self.captureSession = AVCaptureSession()
+        
+        //Setting a device
+        guard let device = AVCaptureDevice.default(for: .video) else {return}
+        let input : AVCaptureDeviceInput
+        do{
+            input = try AVCaptureDeviceInput(device:device)
+        }catch{
+            return
+        }
+        if captureSession.canAddInput(input){
+            captureSession.addInput(input)
+        }
+        else{
+            //Muestra la alerta de error
+            return
+        }
+        
+        //Setting the output
+        let metadadaOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metadadaOutput){
+            captureSession.addOutput(metadadaOutput)
+            metadadaOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadadaOutput.metadataObjectTypes = [.qr]
+        }
+        else{
+            //alerta aquí
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        // Fix rotation
+        previewLayer.frame = self.view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(previewLayer)
+        
+        // Add drawn sqare to the reader.
+        self.addSquareToQrReader()
+        
+        // Set the read area to the rectangle.
+        self.setQrReaderArea(metadata: metadadaOutput, previewLayer: previewLayer)
+        
+        captureSession.commitConfiguration()
+        captureSession.startRunning()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if captureSession.isRunning == false{
+            captureSession.startRunning()
+        }
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        if captureSession.isRunning == true{
+            captureSession.stopRunning()
+        }
+        delegate?.codeReaded(self.readedCodes)
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // Class functions
+    func found(code: String) {
+        DispatchQueue.main.asyncAfter(deadline: .now()){
+            if !self.codes.contains(code) && self.qrView.layer.borderColor != UIColor.red.cgColor{
+                AudioServicesPlayAlertSound(1053)
+                AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                self.qrView.layer.borderColor = UIColor.red.cgColor
+            }
+            else if self.codes.contains(code) && self.qrView.layer.borderColor != UIColor.green.cgColor{
+                // Here goes the code for mark the student
+                AudioServicesPlayAlertSound(1114)
+                self.qrView.layer.borderColor = UIColor.green.cgColor
+                self.readedCodes.append(code)
+            }
+        }
     }
-    */
+    func addSquareToQrReader(){
+        // put a little square in camera
+        qrView = UIView()
+        qrView.frame = QrviewBounds.frame
+        qrView.center = CGPoint(x:self.view.frame.width/2,y:self.view.frame.height/2)
+        QrviewBounds.removeFromSuperview()
+        qrView.layer.borderColor = UIColor.gray.cgColor
+        qrView.layer.borderWidth = 3
+        qrView.layer.cornerRadius = 20
+        self.view.addSubview(qrView)
+        self.view.bringSubview(toFront: qrView)
+    }
+    func setQrReaderArea(metadata : AVCaptureMetadataOutput,previewLayer : AVCaptureVideoPreviewLayer){
+        // Set the read area to the rectangle.
+        let rect = CGRect(x: qrView.center.x-(qrView.bounds.width/2)-20, y: qrView.center.y-(qrView.bounds.height/2)-20, width: qrView.bounds.width+20, height: qrView.bounds.height+20)
+        metadata.rectOfInterest = previewLayer.metadataOutputRectConverted(fromLayerRect: rect)
+    }
 
+}
+
+
+extension QrReaderViewController:AVCaptureMetadataOutputObjectsDelegate{
+    // Read the qr code value
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+        else{
+            if self.qrView.layer.borderColor != UIColor.gray.cgColor{
+                self.qrView.layer.borderColor = UIColor.gray.cgColor
+            }
+        }
+        dismiss(animated: true)
+    }
 }
