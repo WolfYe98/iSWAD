aac62e883567b859547296821f3b51ba0e36bf08
WolfYe98 <mingye@hotmail.es>
2021-04-11 19:23:53 +0200

    Bate Ye: added code for run app views always on light mode

--- iSWAD/iSWAD/AppDelegate.swift
+++ iSWAD/iSWAD/AppDelegate.swift
@@ -24,7 +24,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         if reachability?.connection != Optional.none && reachability?.connection.description != "No Connection"{
             defaults.set(nil, forKey: Constants.wsKey)
         }
-        
+        UIApplication.shared.windows.forEach{window in
+            if #available(iOS 13.0, *) {
+                window.overrideUserInterfaceStyle = .light
+            } else {
+                // Fallback on earlier versions
+            }
+        }
         if (defaults.string(forKey: Constants.wsKey) == nil || defaults.string(forKey: Constants.wsKey) == "") {
             self.window?.rootViewController?.performSegue(withIdentifier: "showLogin", sender: self)
         } else {
@@ -51,20 +57,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                 if !accepted {
                     print("Permiso denegado por el usuario")
                 }
+                UNUserNotificationCenter.current().delegate = self
             }
         }
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
@@ -81,8 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                 content.subtitle = ""
                 content.body = "Tiene \(notifications) notificaciones nuevas en SWAD"
                 content.sound = UNNotificationSound.default()
-                content.setValue(true, forKey: "shouldAlwaysAlertWhileAppIsForeground")
-                
                 // 3. Creamos la Request
                 let request = UNNotificationRequest(identifier: "SWADNotification", content: content, trigger: trigger)
                 
@@ -97,9 +93,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         }
     }
     
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
@@ -121,3 +114,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 }
 
 
+extension AppDelegate: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.sound])
+    }
+}
